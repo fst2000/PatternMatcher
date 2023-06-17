e12886acf10f0d35a79f5b9680dd491e861bfc22
fst2000 <fst.18.work@gmail.com>
2023-06-17 13:14:50 +0400

    AllCharCondition fixed

--- AllCharsConditionReader.java
+++ AllCharsConditionReader.java
@@ -10,8 +10,7 @@ public class AllCharsConditionReader implements Reader
     public void read(CharSequence sequence)
     {
         int length = sequence.length();
-        boolean allHasCondition = true;
-        allHasCondition = length != 0;
+        boolean allHasCondition = length != 0;
         for(int i = 0; i < length; i++)
         {
             if(!condition.check(sequence.charAt(i))) allHasCondition = false;
